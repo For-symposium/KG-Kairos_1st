d16352345959bf8b8cbb82574d6b3bff97b1a77a
zy <ying.zhang@elephantrobotics.com>
2024-04-23 15:20:17 +0800

    modified:   Final_Project/Automobile_car/ydlidar_ros_driver/launch/X2.launch 	new file:   Final_Project/Automobile_car/ydlidar_ros_driver/src/LidarX2.py 	new file:   Final_Project/Automobile_car/ydlidar_ros_driver/src/__pycache__/LidarX2.cpython-311.pyc 	new file:   Final_Project/Automobile_car/ydlidar_ros_driver/src/sample.py 	modified:   Final_Project/Automobile_car/ydlidar_ros_driver/src/ydlidar_gpio.py

--- Final_Project/Automobile_car/ydlidar_ros_driver/src/ydlidar_gpio.py
+++ Final_Project/Automobile_car/ydlidar_ros_driver/src/ydlidar_gpio.py
@@ -4,29 +4,44 @@ import time
 if os.name == "posix":
     import RPi.GPIO as GPIO
 
-def radar_open(self):
-    def radar_high():
-        GPIO.setmode(GPIO.BCM)
-        time.sleep(0.1)
-        GPIO.setup(20, GPIO.OUT)
-        GPIO.output(20, GPIO.HIGH)
+# def lidar_open(self):
+#     def lidar_high():
+#         GPIO.setmode(GPIO.BCM)
+#         time.sleep(0.1)
+#         GPIO.setup(20, GPIO.OUT)
+#         GPIO.output(20, GPIO.HIGH)
 
-    radar_high()
-    time.sleep(0.05)
+#     lidar_high()
+#     time.sleep(0.05)
     # launch_command = "roslaunch myagv_odometry myagv_active.launch"  # 使用ros 打开
     # subprocess.run(
     #     ['gnome-terminal', '-e', f"bash -c '{launch_command}; exec $SHELL'"])
 
-def radar_close(self, run_launch):
-    def radar_low():
-        GPIO.setmode(GPIO.BCM)
-        time.sleep(0.1)
-        GPIO.setup(20, GPIO.OUT)
-        GPIO.output(20, GPIO.LOW)
+def lidar_high():
+    GPIO.setmode(GPIO.BCM)
+    time.sleep(0.1)
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.output(20, GPIO.HIGH)
 
-    radar_low()
-    time.sleep(0.05)
+# def lidar_close(self, run_launch):
+#     def lidar_low():
+#         GPIO.setmode(GPIO.BCM)
+#         time.sleep(0.1)
+#         GPIO.setup(20, GPIO.OUT)
+#         GPIO.output(20, GPIO.LOW)
+
+#     lidar_low()
+#     time.sleep(0.05)
 
     # close_command = "ps -ef | grep -E " + run_launch + \
     #     " | grep -v 'grep' | awk '{print $2}' | xargs kill -2"
     # subprocess.run(close_command, shell=True)
+
+def lidar_low():
+    GPIO.setmode(GPIO.BCM)
+    time.sleep(0.1)
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.output(20, GPIO.LOW)
+
+if __name__ == "__main__":
+    lidar_high()
