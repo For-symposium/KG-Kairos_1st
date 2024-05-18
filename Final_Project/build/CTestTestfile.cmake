9dfebd67853d956b4dcd7c706de3bf20daa1bb83
bhg <han00191925@gmail.com>
2024-05-18 15:58:43 +0900

    Change folder location

--- Final_Project/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bhg/project/KG-Kairos_1st/Final_Project/src
-# Build directory: /home/bhg/project/KG-Kairos_1st/Final_Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("elec_car_charge")
--- Final_Project/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/bhg/project/KG-Kairos_1st/Final_Project/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
