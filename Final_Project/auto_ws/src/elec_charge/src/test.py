089de4f9f5746f208cdcf7e535d8607be485ae9f
bhg <han00191925@gmail.com>
2024-05-29 14:05:37 +0900

    Final ver2.0 before testing

--- Final_Project/auto_ws/src/elec_charge/src/test.py
+++ Final_Project/auto_ws/src/elec_charge/src/test.py
@@ -1,9 +1,15 @@
-def test(a):
-    if a == 0:
-        return "Zero"
-    elif a == 1:
-        return "One"
+import time
+import rospy
+from std_msgs.msg import Int32
 
-while True:
-    a = int(input("Zero or One : "))
-    print(test(a))
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('test_node', anonymous=True)
+        rospy.loginfo("Test node launched")
+        pub_motor = rospy.Publisher('vehicle_position', Int32, queue_size=10)
+        rate = rospy.Rate(10)
+        test_publish_message()
+    except rospy.ROSInterruptException:
+        print("Cam pub node : Stop publishing")
--- Final_Project/auto_ws/src/elec_charge/src/test_control_sub.py
+++ Final_Project/auto_ws/src/elec_charge/src/test_control_sub.py
@@ -17,8 +17,6 @@
 
 - -100 : Cam mode ON, Others OFF
 - -110 : IR mode ON
-- -113 : IR mode ON, Others OFF, Zero-turn Left
-- -117 : IR mode ON, Others OFF, Zero-turn Right
 - -120 : Zero-turn mode ON, Others OFF
 '''
 '''
@@ -43,6 +41,8 @@ i = 0
 cam_mode = True
 zero_turn_mode = False
 zero_turn_dir = 0 # Left(-1), Right(1)
+zero_turn_arr = []
+t_course_cnt = 0
 ser = None
 control_bit = "00000000"  # Initialize control bit
 rate = None  # Define rate globally to use in callbacks
@@ -53,28 +53,23 @@ def cam_motor_control_callback(data):
         if data.data == 10:
             print(f"Cam Sub : GO {i}")
             i += 1
-            # mc.go_ahead(1)
             control_bit = "11110001"
-            send_data(control_bit)
+            # send_data(control_bit)
         elif data.data == 1:
             print(f"Cam Sub : Right {i}")
             i += 1
-            # mc.clockwise_rotation(1)
             control_bit = "11100000"
-            send_data(control_bit)
+            # send_data(control_bit)
         elif data.data == -1:
             print(f"Cam Sub : Left {i}")
             i += 1
-            # mc.counterclockwise_rotation(1)
-            # control_bit = "11010000"
             control_bit = "01010001"
-            send_data(control_bit)
+            # send_data(control_bit)
         elif data.data == 0:
             print(f"Cam Sub : STOP {i}")
             i += 1
-            # mc.stop()
             control_bit = "00110001"
-            send_data(control_bit)
+            # send_data(control_bit)
         rate.sleep()
         if data.data == -110:
             print(f"Cam Sub : Switch to IR mode {i}")
@@ -89,16 +84,14 @@ def IR_motor_control_callback(data):
         if data.data == 10:
             print(f"IR Sub : GO {i}")
             i += 1
-            # mc.go_ahead(1)
             control_bit = "11110001"
-            send_data(control_bit)
+            # send_data(control_bit)
         elif data.data == 0:
             print(f"IR Sub : All white. STOP {i}")
             i += 1
             zero_turn_mode = True
-            # mc.stop()
             control_bit = "00110001"
-            send_data(control_bit)
+            # send_data(control_bit)
         rate.sleep()
     if cam_mode == False and zero_turn_mode == True:
         ##### Zero-turn by hard-coding #####
@@ -106,50 +99,58 @@ def IR_motor_control_callback(data):
             print(f"Zero turn Sub : Turn Left {i}")
             i += 1
             control_bit = "10010001"
-            send_data(control_bit)
+            # send_data(control_bit)
             time.sleep(5)
             zero_turn_mode, cam_mode = False, True
         elif zero_turn_dir == 1:
             print(f"Zero turn Sub : Turn Right {i}")
             i += 1
             control_bit = "10100001"
-            send_data(control_bit)
+            # send_data(control_bit)
             time.sleep(5)
             zero_turn_mode, cam_mode = False, True
         rate.sleep()
 
+def generate_zeroturn_arr(data):
+    global zero_turn_arr, t_course_cnt
+    # Zero turn direction
+    if data.data // 10 == 1: # Left 11, 12, 13
+        zero_turn_arr = [-1, -1, 1, 1] # LLRR
+    elif data.data // 10 == 2: # Right 21, 22
+        zero_turn_arr = [1, 1, -1, -1] # RRLL
+    # T-course count
+    t_course_cnt = data.data % 10
+    pub_t_course_cnt.publish(t_course_cnt)
 
 def clean_up():
     rospy.loginfo("Sub node: Cleaning up...")
-    # mc.stop()
 
 def listener():
-    # rospy.init_node('motor_control_sub_node', anonymous=True)
+    rospy.init_node('motor_control_node', anonymous=True)
     rospy.loginfo("Sub node : Start Subscribing")
     rospy.Subscriber('control_cam', Int32, cam_motor_control_callback, queue_size=10) # line tracing
     rospy.Subscriber('control_IR', Int32, IR_motor_control_callback) # line tracing
+    rospy.Subscriber('vehicle_position', Int32, generate_zeroturn_arr) # receive EV position
     rospy.on_shutdown(clean_up)
-    # rospy.spin()  # Keep away from exiting
+    rospy.spin()  # Keep away from exiting
 
 def send_data(control_bit):
     int_data = int(control_bit, 2)
     byte_data = int_data.to_bytes(1, byteorder='big')
-    ser.write(byte_data)
+    # ser.write(byte_data)
     rospy.loginfo(f"Sending: {bin(int_data)[2:].zfill(8)}")
 
 if __name__ == '__main__':
     try:
         rospy.init_node('motor_control_sub_node', anonymous=True)
         rospy.loginfo(f"serial connetected")
-
         rate = rospy.Rate(10)
-        port = "/dev/ttyUSB0"
-        baudrate = 9600
-        ser = serial.Serial(port, baudrate, timeout=1)
-        thread = threading.Thread(target=send_data(control_bit))
-        thread.start()
+        # port = "/dev/ttyUSB0"
+        # baudrate = 9600
+        # ser = serial.Serial(port, baudrate, timeout=1)
+        # threading.Thread(target=send_data(control_bit)).start()
+        pub_t_course_cnt = rospy.Publisher('t_course_cnt', Int32, queue_size=10) # T-course count
         listener()
-
         rospy.spin()
 
     except rospy.ROSInterruptException:
